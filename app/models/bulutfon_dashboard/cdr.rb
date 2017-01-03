5340cbd10c43b2d9c7bf9d7b0b0e69f77b3173dc
İsmail Akbudak <ismail.akbudak@lab2023.com>
2017-01-03 14:45:58 +0300

    Improve cdr list and detail page

--- app/models/bulutfon_dashboard/cdr.rb
+++ app/models/bulutfon_dashboard/cdr.rb
@@ -4,7 +4,7 @@ module BulutfonDashboard
     include ActiveModel::Conversion
     extend ActiveModel::Naming
 
-    cattr_accessor :uuid, :bf_calltype, :direction, :caller, :callee, :extension, :call_price, :call_time, :answer_time, :hangup_time, :call_record, :hangup_cause, :hangup_state, :call_flow
+    cattr_accessor :uuid, :bf_calltype, :direction, :caller, :callee, :extension, :call_price, :call_time, :answer_time, :hangup_time, :call_record, :hangup_cause, :hangup_state, :call_flow, :caller_id_number, :destination_number, :sip_hangup_disposition
 
     def initialize(attributes = {})
       attributes.each do |name, value|
@@ -16,16 +16,39 @@ module BulutfonDashboard
       false
     end
 
-    def caller_id_number
-
+    def direction_in?
+      direction == 'IN'
     end
 
-    def destination_number
-
+    def direction_out?
+      direction == 'OUT'
     end
 
-    def sip_hangup_disposition
-
+    def hangup_cause_i18n
+      I18n.t("enumerates.hangup_cause.#{hangup_cause.upcase}")
     end
+
+    def sip_hangup_disposition_hover_i18n
+      I18n.t("enumerates.sip_hangup_disposition_hover.#{disposition_state}")
+    end
+
+    def sip_hangup_disposition_i18n
+      I18n.t("enumerates.sip_hangup_disposition.#{disposition_state}")
+    end
+
+    def calculate_call_price
+      call_price.present? ? call_price.to_f * 100 : 0.00
+    end
+
+    def call_record_exists?
+      call_record == 'YES'
+    end
+
+    private
+
+    def disposition_state
+      hangup_state.present? ? hangup_state : 'blank'
+    end
+
   end
 end
